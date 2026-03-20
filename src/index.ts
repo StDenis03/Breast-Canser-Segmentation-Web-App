4120425d4635631475d0a29cb5586b2e1e639241
MityaVit <vitmitya@gmail.com>
2026-03-20 03:06:56 +0300

    Initial

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import './global.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<section id="center">
+  <div>
+    <h1>Hello, World!</h1>
+    <p>Initial load</p>
+  </div>
+</section>
+`
