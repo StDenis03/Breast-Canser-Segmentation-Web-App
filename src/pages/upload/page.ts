a27016975eba81ae778aa6028b3d5cf3ee132fc6
MityaVit <vitmitya@gmail.com>
2026-03-26 12:07:31 +0500

    feat(ui): add upload page base

--- src/pages/upload/page.ts
+++ src/pages/upload/page.ts
@@ -1,12 +1,27 @@
-import style from './style.module.css';
+import Uppy from '@uppy/core';
+import Dashboard from '@uppy/dashboard';
+import styles from './style.module.css';
 
-export default function (root: HTMLElement) {
-  const section = document.createElement('section');
-  section.className = style.center;
-  section.innerHTML =  `
-    <div>
-      <h1>Hello, World!</h1>
-      <p>Initial load</p>
-    </div>`;
-  root.append(section);
+import '@uppy/core/css/style.min.css';
+import '@uppy/dashboard/css/style.min.css';
+
+export default function render(container: HTMLElement) {
+    container.innerHTML = `
+      <section class="${styles.uploadWrapper}">
+          <div class="${styles.glassCard}">
+              <h1 class="${styles.title}">Upload your file</h1>
+              <div id="dashboard"></div>
+          </div>
+      </section>
+    `;
+
+    new Uppy({
+      restrictions: { maxNumberOfFiles: 1 }
+    })
+    .use(Dashboard, {
+      target: '#dashboard',
+      inline: true,
+      height: 350,
+      proudlyDisplayPoweredByUppy: false
+    });
 }
