cadf02cd92d9fedeb1409f71ecc6c0999ed9ad24
Mitya <106722909+MityaVit@users.noreply.github.com>
2026-04-05 17:41:18 +0300

    Revert "feat/frontend: base upload functionality"

--- src/code/frontend/src/features/upload/ui/upload-container.ts
+++
@@ -1,8 +0,0 @@
-import styles from './upload-container.module.css';
-import { t } from '@/shared/i18n';
-
-export const uploadContainerHTML = `
-  <div class="${styles.glassCard}">
-      <h1 class="${styles.title}">${t("upload-file")}</h1>
-      <div id="dashboard"></div>
-  </div>`;
