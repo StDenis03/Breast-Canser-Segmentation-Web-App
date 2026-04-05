4787e4edc27c83e65ea449767217aa24afaa562a
senliica <vitmitya@gmail.com>
2026-04-06 00:41:55 +0300

    feat(ui): add XHRUpload

---
+++ src/code/frontend/src/features/upload/model/create-uppy.ts
@@ -0,0 +1,40 @@
+import Uppy from '@uppy/core';
+import XHRUpload from '@uppy/xhr-upload';
+
+import German from '@uppy/locales/lib/de_DE';
+import French from '@uppy/locales/lib/fr_FR';
+import Russian from '@uppy/locales/lib/ru_RU';
+import Chinese from '@uppy/locales/lib/zh_CN';
+import English from '@uppy/locales/lib/en_US';
+
+import { currentLocale } from '@/shared/i18n';
+
+const uppyLocales: Record<string, any> = {
+  fr: French,
+  de: German,
+  ru: Russian,
+  zh: Chinese,
+  en: English
+};
+
+const allowedFileTypes = ['.dcm', '.dic', '.dicom'];
+
+export function createUploadUppy() {
+  if (!uppyLocales[currentLocale]) {
+    console.warn(`Uppy translations for '${currentLocale}' locale not found.`);
+  }
+
+  return new Uppy({
+    locale: uppyLocales[currentLocale] ?? English,
+    restrictions: {
+      maxNumberOfFiles: 1,
+      allowedFileTypes
+    },
+  }).use(XHRUpload, {
+    endpoint: '/api/upload',
+    fieldName: 'file',
+    formData: true
+  });
+}
+
+export { allowedFileTypes };
