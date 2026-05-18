875dd56a1c18ae393771ff15dc272b945eb76b0a
MityaVit <vitmitya@gmail.com>
2026-05-18 20:31:00 +0300

    feat(cd): set up dc container with base nginx web-server

--- src/code/frontend/vite.config.ts
+++ src/code/frontend/vite.config.ts
@@ -1,36 +1,38 @@
-import { defineConfig, loadEnv } from 'vite';
-import { heyApiPlugin } from '@hey-api/vite-plugin';
-import path from 'path';
+import { defineConfig, loadEnv } from "vite";
+import { heyApiPlugin } from "@hey-api/vite-plugin";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
-    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-    return {
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './src')
-            }
+  return {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    css: {
+      devSourcemap: true,
+      modules: {
+        generateScopedName: "[name]__[local]__[hash:base64:5]",
+      },
+    },
+    build: {
+      sourcemap: true,
+    },
+    server: {
+      port: 3000,
+      open: true,
+      middlewareMode: true,
+    },
+    plugins: [
+      heyApiPlugin({
+        config: {
+          input: `${process.env.VITE_BACKEND_URL}/openapi.json`,
+          output: "src/shared/api/generated",
         },
-        css: {
-            devSourcemap: true,
-            modules: {
-                generateScopedName: '[name]__[local]__[hash:base64:5]',
-            }
-        },
-        build: {
-            sourcemap: true
-        },
-        server: {
-            port: 3000,
-            open: true
-        },
-        plugins: [
-            heyApiPlugin({
-                config: {
-                    input: `${process.env.VITE_BACKEND_URL}/openapi.json`,
-                    output: 'src/shared/api/generated'
-                }
-            })
-        ]
-    }
+      }),
+    ],
+    appType: "spa",
+  };
 });
